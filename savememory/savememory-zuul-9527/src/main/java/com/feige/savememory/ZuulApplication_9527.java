2ae69e5e2b2298e2af9ee4c371fed80233194a50
emptyOVO <gaoyifei040229@163.com>
2023-10-09 21:52:54 +0800

    加入路由网关

---
+++ savememory/savememory-zuul-9527/src/main/java/com/feige/savememory/ZuulApplication_9527.java
@@ -0,0 +1,14 @@
+package com.feige.savememory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableZuulProxy //开启zuul代理
+public class ZuulApplication_9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication_9527.class,args);
+    }
+}
